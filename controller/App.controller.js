0396d598c6b15e9537ba002bbf2e267c363132fe
Prathik M R <65172540+prathikmr7@users.noreply.github.com>
2026-06-11 14:36:13 +0530

    Add App.controller.js

---
+++ controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "com/app/sampleui5auth/controller/BaseController"
+], function(
+    BaseController
+) {
+    "use strict";
+
+    return BaseController.extend("com.app.sampleui5auth.controller.App", {
+        onInit: function() {
+        }
+    });
+});
